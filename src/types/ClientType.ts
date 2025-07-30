e5018ed6b1e7d48f821e0b2e0be1b49093bd8200
mvfsilva.develcode <viniciusfranca09@hotmail.com>
2025-07-29 21:43:07 -0300

    feat: improving code and separating responsibilities

--- src/types/ClientType.ts
+++ src/types/ClientType.ts
@@ -1,6 +1,6 @@
 export type Client = {
   id: string;
   name: string;
-  salary: string;
+  salary: number;
   companyValuation: number;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
   plugins: [
     react(),
